ecbe245982e6002430366220b498fa088518491d
Rowland <rowland.wanderi@student.moringaschool.com>
2024-04-03 10:28:43 +0000

    added a view details page and button.

--- my_project_django/product/views.py
+++ my_project_django/product/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -14,3 +16,19 @@ class LatestProductsList(APIView):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    
+
+class ProductDetail(APIView):
+    #first get the product object
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    #then serialize it
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
